e0f02de012fd679a598decee784244eb8d5b8a34
Okeke Dumaga <93274565+Lonecloud7@users.noreply.github.com>
2022-12-29 10:26:48 +0100

    Create new repo

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text
+        style={{
+          color: "white",
+        }}
+      >
+        THIS IS SO FUCKING AWESOME!!!!
+      </Text>
       <StatusBar style="auto" />
+      {console.log("this shit is fire")}
+      {console.log("AM I ACTUALLY SURE")}
+
     </View>
   );
 }
@@ -13,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
